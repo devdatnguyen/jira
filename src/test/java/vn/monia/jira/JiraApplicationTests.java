4ccba4534077dbdd153c94261b1f6a11c5a36209
devdatnguyen <dev.datnguyen@gmail.com>
2022-07-04 23:20:01 +0700

    Initial commit

---
+++ src/test/java/vn/monia/jira/JiraApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.monia.jira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
